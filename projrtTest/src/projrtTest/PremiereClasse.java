b3b53aca902e128a7ba0414b8698c2b157bade85
yelal <yelal@192.168.1.4>
2019-11-12 01:21:46 +0100

    first commit

---
+++ projrtTest/src/projrtTest/PremiereClasse.java
@@ -0,0 +1,14 @@
+package projrtTest;
+
+public class PremiereClasse {
+
+	public PremiereClasse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Bonjour tout le monde !");
+
+	}
+
+}
